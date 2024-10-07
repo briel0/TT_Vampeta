fb7e6ecf5ff20d17d6d7fff8960a6fc4fd1eba7d
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2024-10-07 12:43:32 -0300

    Add zip

--- Autonomo/src/sensor.cpp
+++ Autonomo/src/sensor.cpp
@@ -13,3 +13,68 @@ void sensor_begin()
 	pinMode(sensorD, INPUT);
 	pinMode(sensorE, INPUT);
 }
+
+void sensor_task(void *pvParameters)
+{
+	while (true)
+	{
+		if (running)
+		{
+			if (IrReceiver.decode())
+			{
+				IrReceiver.resume();
+				if (IrReceiver.decodedIRData.command == 0x2)
+				{
+					digitalWrite(led, 1);
+					stop();
+					ESP.restart();
+				}
+			}
+
+			valueSharpF = digitalRead(sensorF);
+			valueSharpD = digitalRead(sensorD);
+			valueSharpE = digitalRead(sensorE);
+
+			if (valueSharpE)
+			{
+				direc = esq;
+			}
+			else if (valueSharpD)
+			{
+				direc = dir;
+			}
+		}
+		vTaskDelay(1);
+	}
+}
+
+void sensor_test()
+{
+	SerialBT.println("Test Sensors");
+	running = true;
+	for (;;)
+	{
+		SerialBT.printf("ESQ: %d, FEN: %d, DIR: %d\n", valueSharpE, valueSharpF, valueSharpD);
+		delay(15);
+	}
+}
+
+void sensor_test_motor()
+{
+	digitalWrite(stby, 1);
+	SerialBT.println("Test Motors With Sensors");
+	running = true;
+	for (;;)
+	{
+		SerialBT.printf("ESQ: %d, FEN: %d, DIR: %d\n", valueSharpE, valueSharpF, valueSharpD);
+
+		if (valueSharpF)
+			forward(255, 255);
+		else if (valueSharpE)
+			left(80, 80);
+		else if (valueSharpD)
+			right(80, 80);
+		else
+			stop();
+	}
+}
